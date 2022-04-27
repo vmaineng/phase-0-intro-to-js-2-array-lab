ad2eb8106944eac4f203527ef949dfc359ba5086
Mai Vang <vmaineng@gmail.com>
2022-04-26 18:19:11 -0700

    commit tree 0d4ab0b72c9195feb53998083369da53eff546bb

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push("Milo", "Otis", "Garfield");
+    });
+
+    function destructivelyAppendCat(name){
+        cats.push(name);
+    };
+    
+    function destructivelyPrependCat(name){
+        cats.unshift(name);
+    };
+    
+    function destructivelyRemoveLastCat(name){
+        cats.pop(name);
+    };
+    
+    function destructivelyRemoveFirstCat(name){
+        cats.shift(name);
+    };
+
+    function appendCat(name){
+        const appendCat = cats.slice();
+        appendCat.push(name);
+        return appendCat;
+
+    }
+
+    function prependCat(name){
+        const prependCat = cats.slice();
+        prependCat.unshift(name);
+        return prependCat;
+    }
+
+    function removeLastCat(){
+        const removeLastCat = cats.slice();
+        removeLastCat.pop();
+        return removeLastCat;
+    }
+
+    function removeFirstCat(){
+        const removeFirstCat = cats.slice();
+        removeFirstCat.shift();
+        return removeFirstCat;
+    }
